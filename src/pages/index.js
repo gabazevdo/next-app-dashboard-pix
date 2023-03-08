b2166d66304927484d89f4d17464731adba52533
Gabriel Azevedo <gbldja@gmail.com>
2023-03-08 16:44:09 -0300

    chore: import components sidebar and card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
-import ChartComponent from '@/components/chart/Chart'
 import Head from 'next/head'
+import ChartComponent from '@/components/chart/Chart'
+import SideBar from '@/components/sidebar'
+import Card from '@/components/card'
+import { Container, ContainerCard, Header } from '@/styles/style-index'
 
 export default function Home() {
   return (
@@ -11,8 +14,19 @@ export default function Home() {
         <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
         <script src="https://cdn.jsdelivr.net/npm/react-apexcharts"></script>
       </Head>
-      <h1>teste</h1>
-      <ChartComponent />
+
+      <Header />
+      <SideBar />
+      <Container>
+        <ContainerCard>
+          <Card total={'R$ 5.000,00'} />
+          <Card total={'R$ 15.000,00'} />
+          <Card total={'R$ 20.000,00'} />
+        </ContainerCard>
+        <div style={{ marginLeft: '30rem' }}>
+          <ChartComponent />
+        </div>
+      </Container>
     </>
   )
 }
