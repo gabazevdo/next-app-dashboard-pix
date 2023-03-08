e63c97662eba1e543463870b9d9585de5e5adecd
Gabriel Azevedo <gbldja@gmail.com>
2023-03-08 11:42:39 -0300

    style: inserting default colors from the Zemo project

---
+++ src/styles/theme.jsx
@@ -0,0 +1,147 @@
+const theme = {
+  colors: {
+    white: '#fff',
+    shadow: {
+      main: 'rgba(0, 0, 0, 0.1)', // modal
+      tooltip: 'rgba(0, 0, 0, 0.25)',
+      arrow: 'rgba(0, 0, 0, 0.18)',
+      100: 'rgba(0, 36, 70, 0.28)', // button action title
+    },
+    hover: {
+      blue: '#0053cf', // checkbox
+      light: '#e0eaf3', // button action
+      list: '#CBD1DE', // lista auto complete
+      tooltip: '#004aba',
+      float: '#005e9b', // float button
+    },
+    border: {
+      modal: '#E5E5E5', // borda do modal
+    },
+    scroll: {
+      track: '#DDDEE0',
+      thumb: '#9DA1B1',
+      hover: '#9DB1C1',
+    },
+    switch: {
+      disabled: '#DDDEE0',
+      normal: '#B9BECE',
+      dot: '#989BA6',
+    },
+    background: {
+      main: '#F9F9FA', // fundo da aplicação
+      box: '#f4f6f8', // caixa do tittle button action
+      close: '#A6A8AB', // X do modal
+      overlay: 'rgba(116, 120, 141, 0.45)', // fundo do modal
+      line: '#CED4DA', // linha do input
+      input: '#edf1f5', // bg do input
+      inputMoney: '#D5D9DD', //background da caisa R$ e %
+      inputDisabled: '#EBEBEB', // input disabled
+      autoCompleteBox: '#F4F4F4', // caixa do input auto complete
+      pagination: '#D7E1EB',
+    },
+    primary: {
+      100: '#6AB5FF',
+      200: '#4CA7FF',
+      300: '#2E98FF',
+      main: '#1089FF',
+      400: '#0078ED',
+      500: '#0067CB',
+      600: '#0056A9',
+      700: '#004588',
+      800: '#003366',
+    },
+    secondary: {
+      100: '#bfc3d0',
+      200: '#999fb3',
+      300: '#737c97',
+      400: '#4d587a',
+      500: '#26345e',
+      main: '#001041',
+      700: '#000e37',
+      800: '#000b2e',
+      900: '#000924',
+      // 100: '#21A8FF',
+      // 200: '#0094F4',
+      // 300: '#0079C7',
+      // main: '#005E9B',
+      // 400: '#005288',
+      // 500: '#004774',
+      // 600: '#003B61',
+      // 700: '#002F4E',
+      // 800: '#00233A',
+    },
+    due: {
+      100: '#C196EC',
+      200: '#B481E8',
+      300: '#A86CE4',
+      main: '#9B57E0',
+      400: '#8736DA',
+      500: '#7424C5',
+      600: '#611EA4',
+      700: '#4D1883',
+      800: '#3A1262',
+    },
+    text: {
+      main: '#333333',
+      light: '#FFFFFF',
+      subText: '#848484',
+      secondary: '#495057',
+      secondaryText: '#52575C',
+      placeholder: '#CACCCF',
+      box: '#525771', // place holder do input e title button action e da label
+      disabled: '#d2d2d2',
+      input: '#1F212C', //text do input
+      inputDisabled: '#919293', //cor do input desabilitado
+      inputMoney: '#3B405C', //caixa do money
+      inputIcon: '#757575', // icon do search bar
+      icon: '#64686B', // setinha eu acho
+      textDisabled: '#737373', // texto do calendario desabilitado
+    },
+    error: {
+      100: '#FF848A',
+      200: '#FF6B73',
+      300: '#FF535B',
+      main: '#FF3A44',
+      400: '#FF131F',
+      500: '#EB000C',
+      600: '#C4000A',
+      700: '#9D0008',
+      800: '#750006',
+    },
+    warning: {
+      100: '#FFAC8B',
+      200: '#FF9B74',
+      300: '#FF8B5C',
+      main: '#FF7A45',
+      400: '#FF5D1D',
+      500: '#F34500',
+      600: '#CB3A00',
+      700: '#A22E00',
+      800: '#7A2300',
+    },
+    success: {
+      100: '#2BFF67',
+      200: '#00FF48',
+      300: '#00D53C',
+      main: '#00AA30',
+      400: '#00952A',
+      500: '#008024',
+      600: '#006A1E',
+      700: '#005518',
+      800: '#004012',
+    },
+    link: {
+      100: '#c8d0fb',
+      200: '#a7b3f8',
+      300: '#8797f6',
+      400: '#667bf3',
+      500: '#455ef1',
+      main: '#2442EE',
+      700: '#1f38ca',
+      800: '#192ea7',
+      900: '#142483',
+    },
+  },
+};
+
+export default theme;
