dfee5ce73b60857585458950498ee0927b56579f
Gabriel Azevedo <gbldja@gmail.com>
2023-03-15 09:55:07 -0300

    style: alter global style add :root

--- src/styles/globalStyle.jsx
+++ src/styles/globalStyle.jsx
@@ -2,19 +2,32 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
 
-html,
-body {
-  @import url("./fonts.css");
+
+:root {
   font-size: 62.5%;
-  font-family: Rubik, sans-serif;
+
+  svg {
+    font-size: 2.4rem;
+  }
+}
+    
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  
+  scroll-behavior: smooth;
+}
+
+html,
+body {
+  @import url("./fonts.css");
+  font-family: Rubik, sans-serif;
   text-decoration: none;
+  background: #f9f9f9;
   scroll-behavior: smooth;
  }
 
+ 
  h1, h2, h3, h4, h5, h6 {
   color: ${({ theme }) => theme.colors.secondary.main};
   font-weight: 600;
