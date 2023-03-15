cc918b7037a6f36b4ef01da911cfd47ea5ec61d9
Gabriel Azevedo <gbldja@gmail.com>
2023-03-15 09:53:27 -0300

    chore: insert Head end meta data

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import GlobalStyle from '@/styles/globalStyle'
 import defaultTheme from '@/styles/theme'
 import { ThemeProvider } from 'styled-components'
@@ -5,6 +6,18 @@ import { ThemeProvider } from 'styled-components'
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Zemo Bank - Relatório Pix</title>
+        <meta
+          property="og:viewport"
+          key="viewport"
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+          link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      </Head>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
         <Component {...pageProps} />
