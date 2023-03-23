9da7601b566756c46fe43eb951487f148ad92b2e
Gabriel Azevedo <gbldja@gmail.com>
2023-03-23 09:40:27 -0300

    fix: correcting error hidratation on Next

--- src/styles/style-index.jsx
+++ src/styles/style-index.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Container = styled.body`
+export const Container = styled.div`
   height: calc(100vh - 10rem);
   width: 100%;
   display: flex;
